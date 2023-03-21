d04492b895c0fcf651e8c0f2f64a8fe6af4a5aa4
mBartal88 <92155142+mBartal88@users.noreply.github.com>
2023-03-21 18:15:53 +0100

    Update database_common.py

--- database_common.py
+++ database_common.py
@@ -4,23 +4,26 @@ import psycopg2.extras
 
 
 def get_connection_string():
-    return os.environ.get('DATABASE_URL')
-    # user_name = os.environ.get('PSQL_USER_NAME')
-    # password = os.environ.get('PSQL_PASSWORD')
-    # host = os.environ.get('PSQL_HOST')
-    # database_name = os.environ.get('PSQL_DB_NAME')
-    #
-    # env_variables_defined = user_name and host and database_name
-    #
-    # if env_variables_defined:
-    #     return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-    #         user_name=user_name,
-    #         password=password,
-    #         host=host,
-    #         database_name=database_name
-    #     )
-    # else:
-    #     raise KeyError('Some necessary environment variable(s) are not defined')
+    # IN CASE DATABASE CLIENT RETURN ONLY THE URL
+    # return 'DATABASE_URL'
+    
+    #FOR LOCAL USAGE
+    user_name = 'PSQL USERNAME'
+    password = 'PSQL PASSWORD'
+    host = 'PSQL HOST'
+    database_name = 'PSQL DATABASE'
+    
+    creditentials_defined = user_name and password and host and database_name
+    
+     if creditentials_defined:
+         return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+             user_name=user_name,
+             password=password,
+             host=host,
+             database_name=database_name
+         )
+     else:
+         raise KeyError('Some necessary environment variable(s) are not defined')
 
 
 def open_database():
